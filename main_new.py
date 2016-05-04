35e9b9cae8ac17489160ad51e61b2a20caa9499e
Ivan Esterajher <ivan.esterajher@gmail.com>
2016-05-04 13:10:17 +0200

    A few solutions

--- main_new.py
+++ main_new.py
@@ -2,31 +2,6 @@
 
 import requests, bs4, json, sys
 
-# sysargv[1] = URL
-
-
-class WebData(object):
-	def __init__(self):
-		self.r = requests.get(sys.argv[1])
-		self.soup = bs4.BeautifulSoup(r.text, 'lxml')
-
-class Oglas(WebData):
-	def __init__(self, pid, title, price):
-		self.id = pid
-		self.title = title
-		self.price = price
-		super().__init__()
-
-class UrlResultSet(object):
-	def __init__(self):
-		self.results = []
-
-	def addOglas(self, oglas):
-		self.results.append(oglas)
-
-
-
-
 
 url = 'http://www.njuskalo.hr/ps4-konzole'
 r = requests.get('http://www.njuskalo.hr/ps4-konzole')
@@ -40,14 +15,12 @@ for link in soup.find_all('li', class_="EntityList-item--Regular"):
 	print("Link: http://www.njuskalo.hr" + link.article.h3.a.get("href"))
 	for price in link.find_all(class_="price"):
 		print("Cijena je: " + price.text + "\n")
-		dataList[url][]
-		ads = {
+		dataList[url] = {
 			'title': link.article.h3.a.string, 
 			'id': link.article.h3.a.get("name"),
 			'price': price.text
 			}
 
-dataList[url] = ads
 print(dataList)
 #data = open("data.p", "w")
 #data.write(json.dumps(dataList))
