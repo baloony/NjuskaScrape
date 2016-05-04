5551caf626269cc04aab26930060ba84e0033c42
Ivan Esterajher <ivan.esterajher@gmail.com>
2016-05-04 13:58:34 +0200

    Cleaned up files, used classes

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import requests, bs4, json, sys
 # sysargv[1] = URL
 
 
-class WebData(object):
-	def __init__(self):
-		self.r = requests.get(sys.argv[1])
-		self.soup = bs4.BeautifulSoup(r.text, 'lxml')
+#class WebData(object):
+#	def __init__(self):
+#		self.r = requests.get(sys.argv[1])
+#		self.soup = bs4.BeautifulSoup(r.text, 'lxml')
 
-class Oglas(WebData):
+class Oglas(object):
 	def __init__(self, pid, title, price):
 		self.id = pid
 		self.title = title
@@ -24,6 +24,21 @@ class UrlResultSet(object):
 	def addOglas(self, oglas):
 		self.results.append(oglas)
 
+class DB(object):
+    def __init__(self):
+        self.oglaslist = {}
+
+    def addresultset(self, url, resultset):
+        self.oglaslist[url] = resultset
+
+    def printinfo(self):
+        for k in self.oglaslist:
+            print("url:" + k)
+
+        
+
+        
+
 
 
 
@@ -32,22 +47,21 @@ url = 'http://www.njuskalo.hr/ps4-konzole'
 r = requests.get('http://www.njuskalo.hr/ps4-konzole')
 data = r.text
 soup = bs4.BeautifulSoup(data, 'lxml')
-dataList = {}
+urlResult = UrlResultSet()
+db = DB()
 
 for link in soup.find_all('li', class_="EntityList-item--Regular"):
+
 	print("##### " + link.article.h3.a.string + " ##### ")
 	print("ID oglasa je: " + link.article.h3.a.get("name"))
 	print("Link: http://www.njuskalo.hr" + link.article.h3.a.get("href"))
 	for price in link.find_all(class_="price"):
 		print("Cijena je: " + price.text + "\n")
-		dataList[url][]
-		ads = {
-			'title': link.article.h3.a.string, 
-			'id': link.article.h3.a.get("name"),
-			'price': price.text
-			}
+		o = Oglas(link.article.h3.a.get, link.article.h3.a.get("name"), link.article.h3.a.get("href"))
+		urlResult.addOglas(o)
 
-dataList[url] = ads
-print(dataList)
+db.addresultset(url, urlResult)
+db.printinfo()
+
 #data = open("data.p", "w")
 #data.write(json.dumps(dataList))
